a4288fe9710b056198ff079198097f4bee41732a
Vadim Kireev <vadim.kireev.89@gmail.com>
2021-02-18 21:14:10 +0500

    Добавьте файлы проекта.

---
+++ SevenLesson/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SevenLesson
+{
+    public struct Note
+    {
+        private int number;
+        public int Number { get; set; }
+
+        private string author;
+        public string Author { get; set; }
+
+        private string Description;
+        public string description { get; set; }
+
+        private string Importent;
+        public string importent { get; set; }
+
+        private DateTime DateDesc;
+        public DateTime dateDesc { get; set; }
+    }
+}
---
+++ SevenLesson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SevenLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Note note
+        }
+    }
+}
